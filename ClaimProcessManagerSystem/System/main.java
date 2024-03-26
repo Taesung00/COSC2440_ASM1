c5cab4539d21ef99a7b0293197ad0914062c55a9
S3847581@rmit.edu.vn <S3847581@rmit.edu.vn>
2024-03-26 15:42:38 +0700

    Order + add attributes

---
+++ ClaimProcessManagerSystem/System/main.java
@@ -0,0 +1,24 @@
+package System;
+
+import Components.Claim;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import java.io.IOException;
+
+import Functions.*;
+import Components.*;
+
+/**
+ * @author <Taesung Yoon - S3847581>
+ */
+
+public class main {
+    public static void main(String[] args) throws IOException, ParseException {
+        Customer c2 = new Customer(12453,"Huy");
+        Load l1 = new Load();
+        l1.printAllFilesStartWith("C-");
+    }
+}
