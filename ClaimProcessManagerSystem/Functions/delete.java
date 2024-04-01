b13129fbb0854d1f86232cea44aa10abe3278431
S3847581@rmit.edu.vn <S3847581@rmit.edu.vn>
2024-04-01 20:43:46 +0700

    implement delete

---
+++ ClaimProcessManagerSystem/Functions/delete.java
@@ -0,0 +1,19 @@
+package Functions;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class delete {
+        public void delete(String Object,String ID) {
+            String projectRoot = System.getProperty("user.dir");
+            String path = projectRoot + "/ClaimProcessManagerSystem/Components" + "/Data/" +Object;
+            try {
+                Path filePath = Paths.get(ID);
+                Files.deleteIfExists(filePath);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+    }
+}
